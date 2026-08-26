2e47bdbcca3b2f964962ff78a20b5ab421e69e41
musi_musi634 <musi-musi634@outlook.jp>
2026-08-27 02:11:14 +0900

    ハッカ油の影響を受けたエンティティのみを対象にするように変更

--- src/main/java/com/musimusi634/menthaoilreinforcement/mixin/EntityMixin.java
+++ src/main/java/com/musimusi634/menthaoilreinforcement/mixin/EntityMixin.java
@@ -3,12 +3,12 @@ package com.musimusi634.menthaoilreinforcement.mixin;
 
 import com.musimusi634.menthaoilreinforcement.IMintDamageHolder;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.dice7000.menthaoil.mixin.IMenthaOilVictim;
 
 @Mixin(value = Entity.class, priority = Integer.MAX_VALUE)
 public abstract class EntityMixin implements IMintDamageHolder {
@@ -27,10 +27,11 @@ public abstract class EntityMixin implements IMintDamageHolder {
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void tickInject(CallbackInfo ci){
-
         Entity entity = (Entity) (Object) this;
-        if (entity.tickCount % 10 == 0) {
-            ((IMintDamageHolder) entity).setMintDamage(((IMintDamageHolder) entity).getMintDamage()+1);
+        if (((IMenthaOilVictim) entity).menthaoil$getAffected()) {
+            if (entity.tickCount % 10 == 0) {
+                ((IMintDamageHolder) entity).setMintDamage(((IMintDamageHolder) entity).getMintDamage() + 1);
+            }
         }
     }
 }
